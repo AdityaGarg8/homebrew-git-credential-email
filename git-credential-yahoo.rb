33852d4f247f4d68574971b04c7f705c3bc9631d
Aditya Garg <gargaditya08@live.com>
2025-05-13 22:14:19 +0530

    Add Yahoo

---
+++ git-credential-yahoo.rb
@@ -0,0 +1,71 @@
+class GitCredentialYahoo < Formula
+  include Language::Python::Virtualenv
+  desc "Git credential helper to get OAauth2 token for Yahoo accounts"
+  homepage "https://github.com/AdityaGarg8/git-credential-email"
+  url "https://github.com/AdityaGarg8/git-credential-email/archive/refs/tags/v3.0.1.tar.gz"
+  sha256 "a1dc244604c4b2c81d51c7af0540fd20214e68c7cca22a8ce1e8a477bcb237c3"
+  license "Apache-2.0"
+
+  depends_on "pyqt@6"
+  depends_on "python@3.13"
+
+  on_linux do
+    depends_on "cryptography"
+  end
+
+  resource "jaraco-classes" do
+    url "https://files.pythonhosted.org/packages/06/c0/ed4a27bc5571b99e3cff68f8a9fa5b56ff7df1c2251cc715a652ddd26402/jaraco.classes-3.4.0.tar.gz"
+    sha256 "47a024b51d0239c0dd8c8540c6c7f484be3b8fcf0b2d85c13825780d3b3f3acd"
+  end
+
+  resource "jaraco-context" do
+    url "https://files.pythonhosted.org/packages/df/ad/f3777b81bf0b6e7bc7514a1656d3e637b2e8e15fab2ce3235730b3e7a4e6/jaraco_context-6.0.1.tar.gz"
+    sha256 "9bae4ea555cf0b14938dc0aee7c9f32ed303aa20a3b73e7dc80111628792d1b3"
+  end
+
+  resource "jaraco-functools" do
+    url "https://files.pythonhosted.org/packages/ab/23/9894b3df5d0a6eb44611c36aec777823fc2e07740dabbd0b810e19594013/jaraco_functools-4.1.0.tar.gz"
+    sha256 "70f7e0e2ae076498e212562325e805204fc092d7b4c17e0e86c959e249701a9d"
+  end
+
+  resource "jeepney" do
+    url "https://files.pythonhosted.org/packages/7b/6f/357efd7602486741aa73ffc0617fb310a29b588ed0fd69c2399acbb85b0c/jeepney-0.9.0.tar.gz"
+    sha256 "cf0e9e845622b81e4a28df94c40345400256ec608d0e55bb8a3feaa9163f5732"
+  end
+
+  resource "keyring" do
+    url "https://files.pythonhosted.org/packages/70/09/d904a6e96f76ff214be59e7aa6ef7190008f52a0ab6689760a98de0bf37d/keyring-25.6.0.tar.gz"
+    sha256 "0b39998aa941431eb3d9b0d4b2460bc773b9df6fed7621c2dfb291a7e0187a66"
+  end
+
+  resource "more-itertools" do
+    url "https://files.pythonhosted.org/packages/ce/a0/834b0cebabbfc7e311f30b46c8188790a37f89fc8d756660346fe5abfd09/more_itertools-10.7.0.tar.gz"
+    sha256 "9fddd5403be01a94b204faadcff459ec3568cf110265d3c54323e1e866ad29d3"
+  end
+
+  resource "secretstorage" do
+    url "https://files.pythonhosted.org/packages/53/a4/f48c9d79cb507ed1373477dbceaba7401fd8a23af63b837fa61f1dcd3691/SecretStorage-3.3.3.tar.gz"
+    sha256 "2403533ef369eca6d2ba81718576c5e0f564d5cca1b58f73a8b23e7d4eeebd77"
+  end
+
+  resource "shtab" do
+    url "https://files.pythonhosted.org/packages/5a/3e/837067b970c1d2ffa936c72f384a63fdec4e186b74da781e921354a94024/shtab-1.7.2.tar.gz"
+    sha256 "8c16673ade76a2d42417f03e57acf239bfb5968e842204c17990cae357d07d6f"
+  end
+
+  def install
+    venv = virtualenv_create(libexec, "python3.13")
+    venv.pip_install resources
+
+    # Rewrite shebang to use the virtualenv Python
+    inreplace "git-credential-yahoo", /^#!.*python.*/, "#!#{libexec}/bin/python"
+    chmod 0755, "git-credential-yahoo"
+
+    (libexec/"bin").install "git-credential-yahoo"
+    bin.install_symlink libexec/"bin/git-credential-yahoo"
+  end
+
+  test do
+    assert_match "Usage", shell_output("#{bin}/git-credential-yahoo --help")
+  end
+end

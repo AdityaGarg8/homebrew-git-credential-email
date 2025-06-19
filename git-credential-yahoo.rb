f65db55f2b1df5c727737b2db498fbe83dda3685
Aditya Garg <gargaditya08@live.com>
2025-06-19 12:30:50 +0530

    Update git-credential-yahoo.rb

--- git-credential-yahoo.rb
+++ git-credential-yahoo.rb
@@ -1,6 +1,6 @@
 class GitCredentialYahoo < Formula
   include Language::Python::Virtualenv
-  desc "Git credential helper to get OAauth2 token for Yahoo accounts"
+  desc "Git credential helper to get OAuth2.0 token for Yahoo accounts"
   homepage "https://github.com/AdityaGarg8/git-credential-email"
   url "https://github.com/AdityaGarg8/git-credential-email/archive/refs/tags/v4.6.tar.gz"
   sha256 "c7bdfdd619a33a1a58ccd1c60c7b718c2de4ce3147d9e95969af703820fb4f71"

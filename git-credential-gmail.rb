2706b6a75a5718ccc4e32afda736eac4613fbb58
AdityaGarg8 <85610623+AdityaGarg8@users.noreply.github.com>
2026-01-13 18:54:56 +0000

    [create-pull-request] automated change

--- git-credential-gmail.rb
+++ git-credential-gmail.rb
@@ -33,8 +33,8 @@ class GitCredentialGmail < Formula
   end
 
   resource "jaraco-context" do
-    url "https://files.pythonhosted.org/packages/8d/7d/41acf8e22d791bde812cb6c2c36128bb932ed8ae066bcb5e39cb198e8253/jaraco_context-6.0.2.tar.gz"
-    sha256 "953ae8dddb57b1d791bf72ea1009b32088840a7dd19b9ba16443f62be919ee57"
+    url "https://files.pythonhosted.org/packages/cb/9c/a788f5bb29c61e456b8ee52ce76dbdd32fd72cd73dd67bc95f42c7a8d13c/jaraco_context-6.1.0.tar.gz"
+    sha256 "129a341b0a85a7db7879e22acd66902fda67882db771754574338898b2d5d86f"
   end
 
   resource "jaraco-functools" do

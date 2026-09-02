b6167bd5c04c2b18988fc40a3eb8587221cb5048
AdityaGarg8 <85610623+AdityaGarg8@users.noreply.github.com>
2026-09-02 21:11:51 +0000

    [create-pull-request] automated change

--- git-credential-gmail.rb
+++ git-credential-gmail.rb
@@ -74,8 +74,8 @@ class GitCredentialGmail < Formula
   end
 
   resource "shtab" do
-    url "https://files.pythonhosted.org/packages/c9/3e/68e4adc2af22d3ff0c4967d52aaac825d33c20467bfd20348526f925e868/shtab-1.12.0.tar.gz"
-    sha256 "46d0b07e6820e3eaaaf95e437a6e89f66a6141dd8bd92da15321939ac692d44d"
+    url "https://files.pythonhosted.org/packages/ef/71/ddb3c0a7a86db44d2fb3f9cbac162f7ddbcbf563b4a174963ba2b3d4d819/shtab-1.12.1.tar.gz"
+    sha256 "0637338723a8fc08ed1c2fd826d8432229924649c26e3247bb48c53d60ca3bf9"
   end
 
   resource "urllib3" do

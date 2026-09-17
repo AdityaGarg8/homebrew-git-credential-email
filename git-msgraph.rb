4a2bf4d8686a18630e9274fdf4c1bd69022624d6
AdityaGarg8 <85610623+AdityaGarg8@users.noreply.github.com>
2026-09-17 21:36:28 +0000

    [create-pull-request] automated change

--- git-msgraph.rb
+++ git-msgraph.rb
@@ -24,8 +24,8 @@ class GitMsgraph < Formula
   end
 
   resource "idna" do
-    url "https://files.pythonhosted.org/packages/5f/f7/abb373e5757eaec4b922b92f97ec8d6d7e057cf06778247604fbc4e7c3f3/idna-3.19.tar.gz"
-    sha256 "5e0811a4383b21dc5838069f801c4fb62113b7447663d2530d2bd6e77b49bf15"
+    url "https://files.pythonhosted.org/packages/f5/08/8eea9d4b8302028f3abb2c0813953f7aec26d33b7a8960ed760e65ff29fa/idna-3.20.tar.gz"
+    sha256 "a7db850025b95ded1eae8a46181a1a6c56c92c96f0e2b005d9ff8dc0210cab44"
   end
 
   resource "jaraco-classes" do
